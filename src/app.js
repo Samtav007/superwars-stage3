8737366a1a302bc269afac1aeda2439eebae5b21
Samtav007 <144683044+Samtav007@users.noreply.github.com>
2024-03-28 13:29:12 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,78 @@
+const PLAYERS = [
+    "Spiderman",
+    "Captain America",
+    "Wonderwoman",
+    "Popcorn",
+    "Gemwoman",
+    "Bolt",
+    "Antwoman",
+    "Mask",
+    "Tiger",
+    "Captain",
+    "Catwoman",
+    "Fish",
+    "Hulk",
+    "Ninja",
+    "Black Cat",
+    "Volverine",
+    "Thor",
+    "Slayer",
+    "Vader",
+    "Slingo"
+];
+
+// initialize players with image and strength
+const initPlayers = (players) => {
+    let detailedPlayers = [];
+
+    // Instead of forloop use Map method
+    // Code here
+
+    players.forEach((player, index) => {
+        detailedPlayers.push({
+            name: player,
+            strength: (2 + index),
+            image: 'images/super-' + (index + 1) + '.png',
+            type: index % 2 == 0 ? "hero" : "villain",
+            
+        })
+    });
+    return detailedPlayers;
+}
+
+// getting random strength
+const getRandomStrength = () => {
+    return Math.ceil(Math.random() * 100);
+}
+
+// Build player template
+const buildPlayers = (players, type) => {
+    let fragment = '';
+
+    // Instead of using for loop
+    // Use chaining of Array methods - filter, map and join
+    // Type your code here
+    players.filter(player => player.type === type)
+        .map(player => {
+            fragment+=`
+            <div class="player">
+                <img src="${player.image}" alt="" >
+                <div class="name">${player.name}</div>
+                <div class="strength">${player.strength}</div>
+            </div>
+        `
+        })
+
+    return fragment;
+}
+
+// Display players in HTML
+const viewPlayers = (players) => {
+    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
+    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
+}
+
+
+window.onload = () => {
+    viewPlayers(initPlayers(PLAYERS));
+}
